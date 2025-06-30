7fc5e93990950bbaf047c6f1edca7abd49d8cda0
Paul Lizer <paullizer@microsoft.com>
2025-06-30 11:18:51 -0400

    Removed external
    these are pending release and will show up in next version.

--- application/single_app/route_external_health.py
+++ application/single_app/route_external_health.py
@@ -12,9 +12,3 @@ def register_route_external_health(app):
         now = datetime.now()
         time_string = now.strftime("%Y-%m-%d %H:%M:%S")
         return time_string, 200
-
-    @app.route('/external/testaccesstoken', methods=['POST'])
-    @accesstoken_required
-    def test_access_token():
-        message = "Access token is valid."
-        return message, 200
